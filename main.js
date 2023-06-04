0c4278c190f5efc5709e29fcfb72b3faecfd4a93
Tomas Aldecoa <toomasald7@gmail.com>
2023-06-04 19:19:49 -0300

    probando cositas

---
+++ main.js
@@ -0,0 +1,8 @@
+let Equipo = prompt("Es usted hincha de Ferro")
+console.log(Equipo)
+
+if(Equipo == "si") {
+    console.log("Felicidades es usted hincha de Ferro, puede acceder.")
+}else{
+    console.log("Ooh, lamentamos informarle que usted no puede entrar a nuestra pagina")
+}
